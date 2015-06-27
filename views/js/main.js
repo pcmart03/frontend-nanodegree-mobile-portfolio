75f97036408d0aa2fe5a982fee982fce141cd40f
Patrick Martin <pcmart03@gmail.com>
2015-06-27 18:07:05 -0400

    finished making corrections based on Udacity feedback

--- views/js/main.js
+++ views/js/main.js
@@ -497,7 +497,7 @@ function updatePositions() {
   var phase;
   var scrollPosition = document.body.scrollTop / 1250;
   for (var i = 0, len = items.length; i < len; i++) {
-    phase = Math.sin(scrollPosition + i % 10) * 1000 + 'px';
+    phase = Math.sin(scrollPosition + i % 5) * 100 + 'px';
     items[i].style.transform = "translateX(" + phase + ")";
   }
 
@@ -517,16 +517,18 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
+  var height = window.innerHeight / s;
+  var pizzas = height * cols
   //Optimization: Reduced the number of sliding pizzas to 200.
-  for (var i = 0; i < 40; i++) {
+  for (var i = 0; i < pizzas; i++) {
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
-    elem.basicLeft = (i % cols) * s;
+    elem.style.left = (i % cols) * s + 'px';
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    document.getElementById("movingPizzas1").appendChild(elem);
   }
   updatePositions();
 });
